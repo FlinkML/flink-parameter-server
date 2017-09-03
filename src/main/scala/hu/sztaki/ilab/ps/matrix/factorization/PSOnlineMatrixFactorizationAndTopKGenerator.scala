5f5164e56dc7a2cf21720e6619ab19348de9c50a
bereczdaniel <berecz.17@gmail.com>
2017-09-03 13:45:39 +0200

    Online MF and Top K Generator: generates top k for each input and use them as train input

---
+++ src/main/scala/hu/sztaki/ilab/ps/matrix/factorization/PSOnlineMatrixFactorizationAndTopKGenerator.scala
@@ -0,0 +1,102 @@
+package hu.sztaki.ilab.ps.matrix.factorization
+
+import hu.sztaki.ilab.ps.{FlinkParameterServer, WorkerLogic}
+import hu.sztaki.ilab.ps.matrix.factorization.factors.{RangedRandomFactorInitializerDescriptor, SGDUpdater}
+import hu.sztaki.ilab.ps.matrix.factorization.pruning.{LEMPPruningStrategy, LI}
+import hu.sztaki.ilab.ps.matrix.factorization.utils.{CollectTopKFromEachWorker, IDGenerator, Rating, RichRating}
+import hu.sztaki.ilab.ps.matrix.factorization.utils.Utils.{ItemId, UserId}
+import hu.sztaki.ilab.ps.matrix.factorization.utils.Vector._
+import hu.sztaki.ilab.ps.matrix.factorization.workers.PSOnlineMatrixFactorizationAndTopKGeneratorWorker
+import hu.sztaki.ilab.ps.server.SimplePSLogic
+import org.apache.flink.api.common.functions.{Partitioner, RichFlatMapFunction}
+import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.util.Collector
+
+
+class PSOnlineMatrixFactorizationAndTopKGenerator{
+
+}
+
+/**
+  * A use-case for PS: online matrix factorization with SGD and TopK generator with LEMP
+  * The item vectors are stored at the workers and the user vectors at the parameter server.
+  * Each rating in the Stream is broadcasted for every worker where everyone generates a local top k
+  * for the given user. The local top k-s will be merged in the sink.
+  * After the top k generation the worker node, where the item can be found, will calculate the SGD step
+  * and pushing the updated parameter.
+  *
+  */
+object PSOnlineMatrixFactorizationAndTopKGenerator {
+
+  /**
+    *
+    * @param src                A flink data stream containing [[hu.sztaki.ilab.ps.matrix.factorization.utils.Rating]]s
+    * @param numFactors         Number of latent factors
+    * @param rangeMin           Lower bound of the random number generator
+    * @param rangeMax           Upper bound of the random number generator
+    * @param learningRate       Learning rate of SGD
+    * @param negativeSampleRate Number of negative samples (Ratings with rate = 0) for each positive rating
+    * @param userMemory         The last #memory item seen by the user will not be recommended
+    * @param K                  Number of items in the generated recommendation
+    * @param workerK Number of items in the locally generated recommandations
+    * @param bucketSize Parameter of the LEMP algorithm
+    * @param pruningAlgorithm Pruning strategy based on the LEMP paper
+    * @param pullLimit  Upper limit of unanswered pull requests in the system
+    * @param workerParallelism Number of workernodes
+    * @param psParallelism Number of parameter server nodes
+    * @param iterationWaitTime Time without new rating before shutting down the system (never stops if set to 0)
+    * @return For each rating a (UserId, ItemId, TimeStamp, TopK List) tuple
+    */
+  def psOnlineLearnerAndGenerator(src: DataStream[Rating],
+                                  numFactors: Int = 10,
+                                  rangeMin: Double = -0.001,
+                                  rangeMax: Double = 0.001,
+                                  learningRate: Double,
+                                  negativeSampleRate: Int,
+                                  userMemory: Int = 65535,
+                                  K: Int = 100,
+                                  workerK: Int = 75,
+                                  bucketSize: Int = 100,
+                                  pruningAlgorithm: LEMPPruningStrategy = LI(5, 2.5),
+                                  pullLimit: Int = 500,
+                                  workerParallelism: Int = 4,
+                                  psParallelism: Int = 4,
+                                  iterationWaitTime: Long = 10000): DataStream[(UserId, ItemId, Long, List[(Double, ItemId)])] = {
+
+
+    val factorInitDesc = RangedRandomFactorInitializerDescriptor(numFactors, rangeMin, rangeMax)
+
+    val baseWorkerLogic = new PSOnlineMatrixFactorizationAndTopKGeneratorWorker(
+      negativeSampleRate = negativeSampleRate,
+      userMemory = userMemory,
+      workerK = workerK,
+      bucketSize = bucketSize,
+      pruningAlgorithm = pruningAlgorithm,
+      workerParallelism = workerParallelism,
+      factorInitDesc = factorInitDesc,
+      factorUpdate = new SGDUpdater(learningRate))
+
+    val workerLogic = WorkerLogic.addPullLimiter(baseWorkerLogic, pullLimit)
+
+    val serverLogic = new SimplePSLogic[LengthAndVector](
+      x => attachLength(factorInitDesc.open().nextFactor(x)),
+      { (vec, deltaVec) => attachLength(vectorSum(vec._2, deltaVec._2))}
+    )
+
+    val partitionedInput = src.flatMap(new RichFlatMapFunction[Rating, RichRating] {
+      override def flatMap(in: Rating, out: Collector[RichRating]) {
+        val ratingId = IDGenerator.next
+        for (i <- 0 until workerParallelism) {
+          out.collect(in.enrich(i, ratingId))
+        }
+      }
+    }).partitionCustom(new Partitioner[Int] {
+      override def partition(key: Int, numPartitions: Int): ItemId = { key % numPartitions }
+    }, x => x.targetWorker)
+
+    FlinkParameterServer.transform(
+      partitionedInput, workerLogic, serverLogic,workerParallelism, psParallelism, iterationWaitTime)
+      .flatMap( new CollectTopKFromEachWorker(K, userMemory, workerParallelism)).setParallelism(1)
+  }
+
+}
---
+++ src/main/scala/hu/sztaki/ilab/ps/matrix/factorization/workers/PSOnlineMatrixFactorizationAndTopKGeneratorWorker.scala
@@ -0,0 +1,195 @@
+package hu.sztaki.ilab.ps.matrix.factorization.workers
+
+import hu.sztaki.ilab.ps.{ParameterServerClient, WorkerLogic}
+import hu.sztaki.ilab.ps.matrix.factorization.utils.Vector._
+import hu.sztaki.ilab.ps.matrix.factorization.factors.{FactorInitializerDescriptor, FactorUpdater}
+import hu.sztaki.ilab.ps.matrix.factorization.pruning.LEMPPruningFunctions._
+import hu.sztaki.ilab.ps.matrix.factorization.pruning._
+import hu.sztaki.ilab.ps.matrix.factorization.utils.RichRating
+import hu.sztaki.ilab.ps.matrix.factorization.utils.Utils._
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+
+/**
+  * Worker logic for online matrix factorization and Top K generation
+  *
+  * @param negativeSampleRate Number of negative samples (Ratings with rate = 0) for each positive rating
+  * @param userMemory         The last #memory item seen by the user will not be recommended
+  * @param workerK Number of items in the locally generated recommendations
+  * @param bucketSize Parameter of the LEMP algorithm
+  * @param pruningAlgorithm Pruning strategy based on the LEMP paper
+  * @param workerParallelism Number of worker nodes
+  * @param factorInitDesc Specifies how the new latent vectors should be initialized
+  * @param factorUpdate Specifies the SGD update step
+  */
+class PSOnlineMatrixFactorizationAndTopKGeneratorWorker(negativeSampleRate: Int,
+                                                        userMemory: Int,
+                                                        workerK: Int,
+                                                        bucketSize: Int,
+                                                        pruningAlgorithm: LEMPPruningStrategy,
+                                                        workerParallelism: Int,
+                                                        factorInitDesc: FactorInitializerDescriptor,
+                                                        factorUpdate: FactorUpdater)
+  extends WorkerLogic[RichRating, LengthAndVector, TopKWorkerOutput] {
+
+  val model = new mutable.HashMap[ItemId, LengthAndVector]()
+
+  val itemIdsDescendingByLength = new mutable.TreeSet[(Double, ItemId)]()(implicitly[Ordering[(Double, ItemId)]].reverse)
+  val itemIdsBuffer = new ArrayBuffer[ItemId] // needed for negative sample generation, where random access to a random element is required
+  val ratingBuffer = new mutable.HashMap[UserId, mutable.Queue[RichRating]]()
+  val seenList = new mutable.HashMap[UserId, mutable.Queue[ItemId]]
+  val seenSet = new mutable.HashMap[UserId, mutable.HashSet[ItemId]]
+
+  var workerId: Int = -1
+
+  override def onRecv(data: RichRating,
+                      ps: ParameterServerClient[LengthAndVector, TopKWorkerOutput]): Unit = {
+    if (workerId == -1) {
+      workerId = data.targetWorker
+    }
+    ratingBuffer synchronized {
+      ratingBuffer.getOrElseUpdate(data.user, mutable.Queue[RichRating]()).enqueue(data)
+    }
+    ps.pull(data.user)
+  }
+
+  override def onPullRecv(paramId: UserId, userAndLen: LengthAndVector,
+                          ps: ParameterServerClient[LengthAndVector, TopKWorkerOutput]): Unit = {
+    val rate = ratingBuffer synchronized {
+      ratingBuffer(paramId).dequeue()
+    }
+
+    val (userVectorLength, userVector) = userAndLen
+
+    val buckets = itemIdsDescendingByLength.toList.grouped(bucketSize)
+
+    val topK = new mutable.PriorityQueue[(Double, ItemId)]()(implicitly[Ordering[(Double, ItemId)]].reverse)
+
+    // focus coordinate for coord pruning test
+    val focus = Array.range(0, userVector.length)
+      .maxBy { x => userVector(x) * userVector(x) }
+
+    // focus coordinate set for incremental pruning test
+    val focusSet = Array.range(0, userVector.length - 1)
+      .sortBy{ x => -userVector(x) * userVector(x) }
+      .take(pruningAlgorithm match {
+        case INCR(x)=> x
+        case LI(x, _)=> x
+        case _=> 0
+      })
+
+    var currentBucket: List[(Double, ItemId)] = null
+
+    while (buckets.hasNext && {
+      currentBucket = buckets.next
+
+      (topK.length < workerK) || (currentBucket.head._1 * userVectorLength > topK.head._1)
+    }) {
+
+      val theta = if (topK.length < workerK) 0.0 else topK.head._1
+      val theta_b_q = theta / (currentBucket.head._1 * userVectorLength)
+      val vectors = currentBucket.map(x => (x._2, model(x._2)))
+
+      val candidates = vectors.filter(pruningAlgorithm match {
+        case LENGTH() => lengthPruning(theta / userVectorLength)
+        case COORD() => coordPruning(focus, userAndLen, theta_b_q)
+        case INCR(_) => incrPruning(focusSet, userAndLen, theta)
+        case LC(threshold) =>
+          if (currentBucket.head._1 > currentBucket.last._1 * threshold)
+            lengthPruning(theta / userVectorLength)
+          else
+            coordPruning(focus, userAndLen, theta_b_q)
+        case LI(_, threshold) =>
+          if (currentBucket.head._1 > currentBucket.last._1 * threshold)
+            lengthPruning(theta / userVectorLength)
+          else
+            incrPruning(focusSet, userAndLen, theta)
+      })
+
+      for (item <- candidates) {
+        val userItemDotProduct = dotProduct(userVector, item._2._2)
+
+        if (topK.size < workerK) {
+          topK += ((userItemDotProduct, item._1))
+        }
+        else {
+          if (topK.head._1 < userItemDotProduct) {
+            topK.dequeue
+            topK += ((userItemDotProduct, item._1))
+          }
+        }
+      }
+    }
+    ps.output((rate, topK))
+
+    if (rate.item.hashCode() % workerParallelism == workerId) {
+      val numFactors = userVector.length
+
+      val set = seenSet.getOrElseUpdate(rate.user, new mutable.HashSet)
+      if (!(set contains rate.item)) {
+        set += rate.item
+        val list = seenList.getOrElseUpdate(rate.user, new mutable.Queue)
+        list += rate.item
+        if (list.length > userMemory) {
+          set -= list.dequeue()
+        }
+      }
+
+      // negative sample
+      var uDelta = new Vector(numFactors)
+      for (_ <- 0 until Math.min(model.size - set.size, negativeSampleRate)) {
+        var negItemId = itemIdsBuffer(Random.nextInt(itemIdsBuffer.length))
+        var counter = 32
+        while ((counter > 0) && (set contains negItemId)) {
+          negItemId = itemIdsBuffer(Random.nextInt(itemIdsBuffer.length))
+          counter -= 1
+        }
+        if (counter > 0) {
+          val (_, negItemValue) = model(negItemId)
+          val (uuDelta, iDelta) = factorUpdate.delta(0.0, userVector, negItemValue)
+          uDelta = vectorSum(uDelta, uuDelta)
+          updateModelNoInit(negItemId, attachLength(vectorSum(negItemValue, iDelta)))
+        }
+      }
+
+      val (_, itemVector) = model.getOrElse(rate.item, initialize(rate.item))
+      val (userDelta, itemDelta) = factorUpdate.delta(rate.rating, userVector, itemVector)
+
+      updateModelNoInit(rate.item, attachLength(vectorSum(itemVector, itemDelta)))
+
+      ps.push(paramId, (Double.NaN, vectorSum(uDelta, userDelta)))
+    }
+
+  }
+
+
+  def updateModelNoInit(id: ItemId, param: LengthAndVector): Unit = {
+    itemIdsDescendingByLength -= ((model(id)._1, id))
+    val (newLength, _) = param
+    model += ((id, param))
+    itemIdsDescendingByLength += ((newLength, id))
+  }
+
+  // used for initialisation
+  def updateModel(id: ItemId, param: LengthAndVector): Unit = {
+    if (model.contains(id)) {
+      itemIdsDescendingByLength -= ((model(id)._1, id))
+    }
+    else {
+      itemIdsBuffer += id
+    }
+    val (newLength, _) = param
+    model += ((id, param))
+    itemIdsDescendingByLength += ((newLength, id))
+  }
+
+  def initialize(id: ItemId): LengthAndVector = {
+    val lenAndVector = attachLength(factorInitDesc.open().nextFactor(id))
+    updateModel(id, lenAndVector)
+    lenAndVector
+  }
+
+}
