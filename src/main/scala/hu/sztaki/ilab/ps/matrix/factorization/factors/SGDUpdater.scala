dd71754d9e01f523ff253ccdb8d6945d882a0e1b
bdaniel <berecz.17@gmail.com>
2018-10-15 15:21:29 +0100

    SGD update with sigmoid

--- src/main/scala/hu/sztaki/ilab/ps/matrix/factorization/factors/SGDUpdater.scala
+++ src/main/scala/hu/sztaki/ilab/ps/matrix/factorization/factors/SGDUpdater.scala
@@ -1,13 +1,14 @@
 package hu.sztaki.ilab.ps.matrix.factorization.factors
 
+import breeze.numerics.sigmoid
+
 class SGDUpdater(learningRate: Double) extends FactorUpdater {
 
   override def delta(rating: Double, user: Array[Double], item: Array[Double]): (Array[Double], Array[Double]) = {
-    val e = rating - user.zip(item).map { case (x, y) => x * y }.sum
-    val userItem = user.zip(item)
+    val error = sigmoid(rating - user.zip(item).map { case (x, y) => x * y }.sum)
 
-    (item.map(i => learningRate * e * i),
-     user.map(u => learningRate * e * u))
+    (item.map(i => learningRate * error * i),
+      user.map(u => learningRate * error * u))
   }
 
 }
